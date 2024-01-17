f92f35ffc343ee8e921f240d52ea34199e1e466f
jaeuk.lee <dlwodnrsms@naver.com>
2024-01-17 20:33:48 +0900

    t

--- src/components/layout/HeaderLayout.tsx
+++ src/components/layout/HeaderLayout.tsx
@@ -1,14 +1,36 @@
+'use client'
 
+import React, { useState } from 'react';
 
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  MenuOutlined
+} from '@ant-design/icons';
+
+import type { MenuProps } from 'antd';
+import { Button, Menu } from 'antd';
 
 function HeaderLayout(){
+
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
 
   return (
     <>
       <header>
+          <Button type="primary" onClick={toggleCollapsed} >
+            {collapsed ? <MenuOutlined /> : <MenuOutlined /> }
+          </Button>
+        <div>
+
+        </div>
             <div>headerLayout</div>
             <div>test</div>
-            https://demos.pixinvent.com/vuexy-html-admin-template/html/vertical-menu-template-dark/
+            https://demos.pixinvent.com/vuexy-html-admin-template/html/vertical-menu-template-semi-dark/app-ecommerce-dashboard.html
       </header>
      
     </>
