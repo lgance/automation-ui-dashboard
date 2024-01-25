cb2ee00a6df067c89c94147e74946d157af3c366
jaeuk.lee <dlwodnrsms@naver.com>
2024-01-26 02:38:09 +0900

    test

--- src/components/layout/HeaderLayout.tsx
+++ src/components/layout/HeaderLayout.tsx
@@ -1,11 +1,7 @@
 'use client'
 
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import sass from '@/styles/layout/content.template.module.scss';
-=======
 import template from '@/styles/layout/content.template.module.scss';
->>>>>>> a00c17f6f07494490ff0a96d9fa8d5ebff9637af
 
 import {
   MenuUnfoldOutlined,
@@ -26,14 +22,14 @@ function HeaderLayout(){
   return (
     <>
       <header>
-        <div className={sass.layout_menu_toggle}>
+        <div className={template.layout_menu_toggle}>
           <MenuOutlined />
         </div>
-        <div className={sass.header_search_area} >
+        <div className={template.header_search_area} >
           <SearchOutlined height={55} />
         </div>
 
-        <div className={sass.header_right_area}>
+        <div className={template.header_right_area}>
             <QuestionCircleOutlined />
         </div>
           
